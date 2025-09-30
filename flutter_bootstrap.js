1c0a078dae612d89cfb73878865ebecc07f9e9d5
Mentitos <172237541+Mentitos@users.noreply.github.com>
2025-09-29 23:47:07 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2854486587"
+    serviceWorkerVersion: "3835690811"
   }
 });
